859baca742318cfc084bb47103281e14884cf7b1
Mikael Hallendal <mhallendal@gmail.com>
2009-10-25 00:00:06 +0200

    Register global key binding

--- GlossyClockAppDelegate.m
+++ GlossyClockAppDelegate.m
@@ -7,13 +7,44 @@
 //
 
 #import "GlossyClockAppDelegate.h"
+#import <Carbon/Carbon.h>
 
 @implementation GlossyClockAppDelegate
 
 @synthesize window;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application 
+OSStatus
+MyHotKeyHandler(EventHandlerCallRef nextHandler, EventRef theEvent, void *userData)
+{
+    // Do something once the key is pressed
+    NSLog(@"Hot key pressed");
+    return noErr;
+}
+
+- (void)registerHotKeys 
+{
+    EventHotKeyRef hotKeyRef;
+    EventHotKeyID hotKeyID;
+    EventTypeSpec eventType;
+    
+    NSLog(@"Registering hot key");
+    
+    eventType.eventClass = kEventClassKeyboard;
+    eventType.eventKind = kEventHotKeyPressed;
+    
+    hotKeyID.signature = 'htk1';
+    hotKeyID.id = 1;
+
+    InstallApplicationEventHandler(&MyHotKeyHandler, 1, 
+                                   &eventType, NULL, NULL);
+    
+    RegisterEventHotKey(49, cmdKey+controlKey, hotKeyID, 
+                        GetApplicationEventTarget(), 0, &hotKeyRef);
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
+{
+    [self registerHotKeys];
 }
 
 @end
